eb71f213b0fd0d793e606ee51431285a81713f11
monty <amonteolivam@gmail.com>
2021-10-28 14:34:11 +0100

    fix: typos in lotto errors. info

--- contracts/lotto/src/error.rs
+++ contracts/lotto/src/error.rs
@@ -33,7 +33,7 @@ pub enum ContractError {
     #[error("Insufficient gift deposit amount for {0} tickets")]
     InsufficientGiftDepositAmount(u64),
 
-    #[error("Gift ticket amount must be greater than zero")]
+    #[error("Sponsorship amount must be greater than zero")]
     InvalidSponsorshipAmount {},
 
     #[error("Lottery already in progress, wait until the next one begins")]
@@ -45,13 +45,13 @@ pub enum ContractError {
     #[error("There are no deposits to withdraw")]
     InvalidWithdraw {},
 
-    #[error("There are no deposits to withdraw")]
+    #[error("There are no enough funds in the contract for that operation")]
     InsufficientFunds {},
 
     #[error("There are no sponsor deposits to withdraw")]
     InvalidSponsorWithdraw {},
 
-    #[error("The Anchor Pool is smaller than total sponsors, no withdraws allowed")]
+    #[error("The Anchor Sponsor Pool is smaller than total sponsors, no withdraws allowed")]
     InsufficientSponsorFunds {},
 
     #[error("The Anchor Pool is smaller than total deposits, no withdraws allowed")]
@@ -78,7 +78,7 @@ pub enum ContractError {
     #[error("Invalid reserve factor config")]
     InvalidReserveFactor {},
 
-    #[error("Invalid reserve factor config")]
+    #[error("Invalid split factor config")]
     InvalidSplitFactor {},
 
     #[error("Invalid instant withdrawal fee config")]

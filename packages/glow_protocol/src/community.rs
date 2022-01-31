c0215109cc924a932046f195d4db2d27f232b5d0
aesmonty <amonteolivam@gmail.com>
2022-01-31 18:12:33 +0100

    fix: clippy allow large enum variants

--- packages/glow_protocol/src/community.rs
+++ packages/glow_protocol/src/community.rs
@@ -16,6 +16,7 @@ pub struct InstantiateMsg {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
+#[allow(clippy::large_enum_variant)]
 pub enum ExecuteMsg {
     UpdateConfig {
         spend_limit: Option<Uint128>,
